183273b241ef9b17294f114f2536449afd5be132
Egorius188 <postulgin98@mail.ru>
2023-07-11 17:25:03 +0300

    change

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,16 @@ public class Main {
         int begin = 300;
         int replenishment = 5000;
         int bonus;
-        boolean b =replenishment>1000;
-        if (b) {bonus = replenishment/100;} else {bonus=0;}
+        boolean part = replenishment > 1000;
+        if (part) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
 
-        System.out.println("Итоговый счет="+(begin+replenishment+bonus)
+        System.out.println("Итоговый счет=" + (begin + replenishment + bonus)
         );
-        System.out.println("Итоговый бонус="+bonus);
+        System.out.println("Итоговый бонус=" + bonus);
 
 
     }
